d84a229865d55d83ec5e0b6b6be67c2afeeb9eea
Valerii <complexityclass@gmail.com>
2023-11-26 17:53:48 +0000

    Add decision tree py impl

--- pyground/main.py
+++ pyground/main.py
@@ -1,11 +1,19 @@
-import rs_ml 
+import rs_ml
+
 
 def main():
     a = 5
     b = 6
     res = rs_ml.sum_as_string_sevens(a, b)
-    
+
     print(f"result is: {res}")
+
+    a = [1, 2, 3, 4, 5]
+    b = [2, 3, 4, 5, 6]
+
+    c = [x for x in zip(a, b)]
+    print(c)
+
 
 if __name__ == "__main__":
     main()
